a78d21e4263fc9da7a1bfbefbbdd4f906b2df313
Philipp Schemel <philippschemel@live.com>
2020-04-29 12:25:35 +0200

    Add and implement typing animation on landing page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -6,9 +6,17 @@
           <h1 class="title">
             {{ name }}
           </h1>
-          <h2 class="subtitle">
-            Developer
-          </h2>
+          <div class="subtitle">
+            <vue-typed-js
+                    :strings="['Developer, fuck it', 'Developer, hell yeah!']"
+                    class="typed-center"
+                    :smartBackspace="true"
+                    :typeSpeed="50"
+                    :startDelay="750"
+            >
+              <h2 class="subtitle"><span class="typing"></span></h2>
+            </vue-typed-js>
+          </div>
           <hr class="one-eight-seven-bar"/>
           <SocialButton platform="linkedin" url="https://www.linkedin.com/in/philippschemel/" />
           <SocialButton platform="github" url="https://github.com/schempil" />
@@ -21,20 +29,26 @@
 
 <script>
   import SocialButton from "./SocialButton";
+  import {VueTypedJs} from "vue-typed-js";
 
   export default {
     name: 'Welcome',
-    components: {SocialButton},
+    components: {SocialButton, VueTypedJs},
     props: {
       name: String
     }
   }
 </script>
 
-<style>
+<style scoped>
   .one-eight-seven-bar {
     width: 187px;
     text-align: center;
     margin: 1.5rem auto;
   }
+
+  .typed-center {
+    justify-content: center;
+  }
+
 </style>
