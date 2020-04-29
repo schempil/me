c91a951eb3e84983815fac0083a15e2d05e2fb1a
Philipp Schemel <philippschemel@live.com>
2020-04-29 20:26:21 +0200

    Test image distorsion effect

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -22,21 +22,36 @@
           <SocialButton platform="linkedin" url="https://www.linkedin.com/in/philippschemel/" />
           <SocialButton platform="github" url="https://github.com/schempil" />
           <SocialButton platform="instagram" url="https://www.instagram.com/swindle1995/" color="google"/>
+          <hr/>
+
         </div>
       </div>
     </section>
+
+    <div class="my-div" style="min-width: 500px; min-height: 500px;"></div>
+
   </div>
 </template>
 
 <script>
   import SocialButton from "./SocialButton";
   import {VueTypedJs} from "vue-typed-js";
+  import hoverEffect from 'hover-effect';
 
   export default {
     name: 'Welcome',
     components: {SocialButton, VueTypedJs},
     props: {
       name: String
+    },
+
+    mounted: function () {
+      new hoverEffect({
+        parent: document.querySelector('.my-div'),
+        image1: '../assets/test.jpg',
+        image2: '../assets/test2.jpg',
+        displacementImage: '../assets/myDistorsionImage.png'
+      })
     }
   }
 </script>
