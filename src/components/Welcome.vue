5d6643a7c20942c4bcf3dc526d07011136760450
Philipp Schemel <philippschemel@live.com>
2020-04-29 20:38:37 +0200

    Asset test

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -22,21 +22,20 @@
           <SocialButton platform="linkedin" url="https://www.linkedin.com/in/philippschemel/" />
           <SocialButton platform="github" url="https://github.com/schempil" />
           <SocialButton platform="instagram" url="https://www.instagram.com/swindle1995/" color="google"/>
+
           <hr/>
+
+          <img src="../assets/test.jpg" alt="demo-image" />
 
         </div>
       </div>
     </section>
-
-    <div class="my-div" style="min-width: 500px; min-height: 500px;"></div>
-
   </div>
 </template>
 
 <script>
   import SocialButton from "./SocialButton";
   import {VueTypedJs} from "vue-typed-js";
-  import hoverEffect from 'hover-effect';
 
   export default {
     name: 'Welcome',
@@ -46,12 +45,7 @@
     },
 
     mounted: function () {
-      new hoverEffect({
-        parent: document.querySelector('.my-div'),
-        image1: '../assets/test.jpg',
-        image2: '../assets/test2.jpg',
-        displacementImage: '../assets/myDistorsionImage.png'
-      })
+
     }
   }
 </script>
