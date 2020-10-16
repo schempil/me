f5ce07568187380406f913b2e40813c07847809c
Philipp Schemel <philippschemel@live.com>
2020-10-16 17:20:28 +0200

    Project cleanup

--- src/main.js
+++ src/main.js
@@ -4,13 +4,9 @@ import Buefy from 'buefy'
 import './assets/style.scss'
 import VueRouter from "vue-router";
 import Welcome from "./components/Welcome";
-import Work from "./components/Work";
-import Contact from "./components/Contact";
 
 const routes = [
-  { path: '/', component: Welcome, props: { name: 'Philipp Schemel' } },
-  { path: '/work', component: Work},
-  { path: '/contact', component: Contact}
+  { path: '/', component: Welcome, props: { name: 'Philipp Schemel' } }
 ]
 
 const router = new VueRouter({ routes, mode: 'history' })
