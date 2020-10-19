3dfac5d65cca123879579703807526d726e6225b
Philipp Schemel <philippschemel@live.com>
2020-10-19 15:42:47 +0200

    Rename WelcomeRoute

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Welcome.vue'
+import Welcome from '../views/Welcome.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Welcome',
+    component: Welcome
   }
 ]
 
