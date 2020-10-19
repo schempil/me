a3a42a91015b0359cab6ae5d3a18d3c38762eb14
Philipp Schemel <philippschemel@live.com>
2020-10-19 16:06:59 +0200

    Implement second route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
+import CV from "@/views/CV.vue";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: '/',
     name: 'Welcome',
     component: Welcome
+  },
+  {
+    path: '/cv',
+    name: 'CV',
+    component: CV
   }
 ]
 
---
+++ src/views/CV.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>CV here 👋🏼</h1>
+  </div>
+</template>
+
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component({
+  components: { }
+})
+export default class CV extends Vue {
+
+  @Prop() private profile!: Profile;
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
